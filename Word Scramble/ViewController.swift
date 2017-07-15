1ca8c02d423dbfd93a3b43c5d2face9a137cf14b
Sujanth <sujanth1987@gmail.com>
2017-07-15 19:42:05 +1000

    Created nested loop to check answer values

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -57,7 +57,7 @@ class ViewController: UITableViewController {
         let submitAction = UIAlertAction(title: "Submit", style: .default) {
             [unowned self, ac] (action: UIAlertAction) in
             let answer = ac.textFields![0]
-            self.submit = answer(answer: answer.text)
+            self.submit(answer: answer.text!)
         }
         
         ac.addAction(submitAction)
@@ -66,6 +66,30 @@ class ViewController: UITableViewController {
 
     func submit(answer: String) {
         
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        return true
     }
 
 }

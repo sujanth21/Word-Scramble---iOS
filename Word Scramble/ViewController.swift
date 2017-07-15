c4262f2e2b5b63dfe860dff3b57737b78bc6498c
Sujanth <sujanth1987@gmail.com>
2017-07-15 20:31:25 +1000

    Fixed issues and completed business logic

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -67,6 +67,8 @@ class ViewController: UITableViewController {
     func submit(answer: String) {
         
         let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
         
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
@@ -75,16 +77,31 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                }
+                else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
                 }
             }
+            else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+            }
         }
+        else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell the word from '\(title!.lowercased())'!"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
-        return !usedWords.contains(word)
-    }
-    
-    func isOriginal(word: String) -> Bool {
         var tempWord = title!.lowercased()
         
         for letter in word.characters {
@@ -96,6 +113,10 @@ class ViewController: UITableViewController {
         }
         
         return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool {
